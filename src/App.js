c473361b7dfeea060caf3ef21e2a6a4bf1737ef2
casualhuman <abdulrahimjallohaarj@gmail.com>
2024-01-05 12:20:03 +0000

    Removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import { useState } from "react"
 import Header from "./components/Header"
-import FeedbackData from "./data/FeedbackData"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
