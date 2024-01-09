acd9f8752bfd55b36f0c8af86fd36e6ec35ae49d
casualhuman <abdulrahimjallohaarj@gmail.com>
2024-01-09 03:41:20 +0000

    Adjust data to be fetched from backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,19 +1,32 @@
-import {createContext, useState} from 'react'
+import {createContext, useState, useEffect} from 'react'
 import {v4 as uuidv4} from 'uuid'
-import FeedbackData from '../data/FeedbackData'
+
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
 
-    const [feedback, setFeedback] = useState([
-        ...FeedbackData
-    ])
+    const [feedback, setFeedback] = useState([])
 
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
+
+   
+    const fetchFeedback = async() => {
+        const res = await fetch('http://localhost:5000/feedback');
+        const data = await res.json()
+    
+        setFeedback(data)
+        
+    
+    }
+
+    useEffect(() => {
+        fetchFeedback()
+
+    }, [])
 
 
     const deleteFeedback = (id) => {
